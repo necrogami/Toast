f628292cab4ee81b479e01342b8446b7d28c1747
Anton C. Swartz IV <djnecrogami@gmail.com>
2010-11-03 20:37:58 -0400

    Adding Inital File structure

---
+++ frontend/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Toast PHP/ZendFramework Non-blocking IRC Bot.
+ *
+ * @author Anton Swartz IV
+ * @version $Id$
+ * @copyright TaleFi LLC, 3 November, 2010
+ * @package default
+ **/
+
+/**
+ * Define DocBlock
+ **/
---
+++ toast.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Toast IRC Bot
+ * Tools Used: Zend Framework 2.0
+ *             PHP 5.3.3+
+ *
+ * @author Anton Swartz IV
+ * @version $Id$
+ * @copyright TaleFi LLC, 3 November, 2010
+ * @package toast
+ **/
+
+/**
+ * Define DocBlock
+ **/
